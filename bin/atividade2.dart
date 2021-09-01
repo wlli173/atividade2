1e04d37213937d7c16fe6a2e95c715916942523f
willi <willighan173@gmail.com>
2021-09-01 08:39:43 -0300

    Criação_atividade2

---
+++ bin/atividade2.dart
@@ -0,0 +1,9 @@
+import 'package:atividade2/atividade2.dart' as atividade2;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Hello world: ${atividade2.calculate()}!');
+  print('---revisão - estrutura de repetição, listas e funções---');
+  print('Revisão-Listas');
+  print('Lista de alunos');
+}
---
+++ test/atividade2_test.dart
@@ -0,0 +1,8 @@
+import 'package:atividade2/atividade2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
